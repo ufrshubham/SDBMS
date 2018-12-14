81750a0b9434e74dd59360da6368931d070ba289
Ruzaki <ufrshubham@gmail.com>
2018-12-15 00:22:39 +0530

    Initial commit: 1. Added base class for sdbms. 2. Implemented object counter for all sdbms objects.

---
+++ sdbms_base_class.py
@@ -0,0 +1,13 @@
+
+class SdbmsBaseClass:
+    """ Base class for all sdbms classes """
+
+    # class wide counter
+    __object_id = 0
+
+    def __init__(self):
+        SdbmsBaseClass.__object_id += 1
+        self.__id = SdbmsBaseClass.__object_id
+
+    def get_id(self):
+        return self.__id
